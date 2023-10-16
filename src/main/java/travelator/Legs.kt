94c969cad2f44ed2f5df6a08ce93df45b8ff4186
wil.pannell <wil.pannell@gmail.com>
2023-10-16 06:45:11 -0700

    convert parameter to receiver

--- src/main/java/travelator/Legs.kt
+++ src/main/java/travelator/Legs.kt
@@ -5,13 +5,13 @@ import java.time.Duration
 fun longestLegOver(legs: List<Leg>, duration: Duration): Leg? {
   var result: Leg? = null
   for (leg in legs) {
-    if (isLongerThan(leg, duration))
-      if (result == null || isLongerThan(leg, result.plannedDuration)) {
+    if (leg.isLongerThan(duration))
+      if (result == null || leg.isLongerThan(result.plannedDuration)) {
         result = leg
       }
   }
   return result
 }
 
-private fun isLongerThan(leg: Leg, duration: Duration) =
-    leg.plannedDuration.compareTo(duration) > 0
+private fun Leg.isLongerThan(duration: Duration) =
+    plannedDuration.compareTo(duration) > 0
