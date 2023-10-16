3fedb4546bf3d77977fe1a7e0b0de884ec7a68cf
wil.pannell <wil.pannell@gmail.com>
2023-10-15 21:40:39 -0700

    public class Legs to Kotlin

---
+++ src/main/java/travelator/Legs.kt
@@ -0,0 +1,22 @@
+package travelator
+
+import java.time.Duration
+import java.util.*
+
+object Legs {
+  @JvmStatic
+  fun findLongestLegOver(legs: List<Leg>, duration: Duration): Optional<Leg> {
+    var result: Leg? = null
+    for (leg in legs) {
+      if (isLongerThan(leg, duration))
+          if (result == null || isLongerThan(leg, result.plannedDuration)) {
+            result = leg
+          }
+    }
+    return Optional.ofNullable(result)
+  }
+
+  private fun isLongerThan(leg: Leg, duration: Duration): Boolean {
+    return leg.plannedDuration.compareTo(duration) > 0
+  }
+}
